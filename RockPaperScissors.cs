b5a772edabd7ba5d9ebbb1f92cb413cf55e0c448
Csenge Gog <Csenge.Gog@solera.com>
2022-11-18 15:34:43 +0100

    Kata exercise

--- RockPaperScissors.cs
+++ RockPaperScissors.cs
@@ -1,8 +1,41 @@
 ﻿using System;
 
-public class Class1
+namespace RockPaperScissors
 {
-	public Class1()
+	public class RockPaperScissors
 	{
+		public int Play(string player1, string player2)
+		{
+			if (player1 == "Rock")
+			{
+				if (player1 2 == "Scissors")
+						return 1;
+                if (player1 2 == "Paper")
+						return 2;
+                if (player1 2 == "Rock")
+						return 0;
+
+            }
+
+			if (player1 == "Scissors" )
+			{
+				if (player2 == "Rock")
+					return 2;
+                if (player2 == "Paper")
+                    return 1;
+                if (player2 == "Scissors")
+                    return 0;
+            }
+
+            if (player1 == "Paper")
+            {
+                if (player2 == "Rock")
+                    return 1;
+                if (player2 == "Paper")
+                    return 0;
+                if (player2 == "Scissors")
+                    return 2;
+            }
+        }
 	}
 }
